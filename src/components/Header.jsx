638e4335a1b4644d6d90e811ce8dd7584da51220
Victor Koech <vickoch20@gmail.com>
2024-07-23 07:09:55 +0300

    Add src/components/Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Pokémon Memory Game</h1>
+      <p>Click on each Pokémon once. Don't click the same one twice!</p>
+    </header>
+  );
+}
+
+export default Header;
