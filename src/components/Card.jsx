67704170654f5b367aad49e11fba340ede951109
Victor Koech <vickoch20@gmail.com>
2024-07-23 07:18:04 +0300

    Add Card.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import styles from './Card.module.css';
+
+function Card({ card, onClick }) {
+  return (
+    <div className={styles.card} onClick={onClick}>
+      <img src={card.image} alt={card.name} className={styles.cardImage} />
+      <p className={styles.cardName}>{card.name}</p>
+    </div>
+  );
+}
+
+export default Card;
